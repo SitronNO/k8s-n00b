7c5d6b8f4c290fdacf1ee4da8cca73b6dec92d29
Vidar Hoel <vidar.hoel@sysrq.info>
2022-03-02 10:43:58 +0100

    Rename of webservice

--- k8s/expose-deployment.sh
+++ k8s/expose-deployment.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # This works with k3s.
-kubectl expose deployment helloredis --type=LoadBalancer --name=web-service
+kubectl expose deployment helloredis --type=LoadBalancer --name=helloredis-web
 
 # TODO
 # Expose it properly with ingress, træfik, etc..
